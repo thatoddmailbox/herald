562bbaf867e2b86b0fd490f2e98e96417b7e1a75
Alex Studer <alex@studer.dev>
2020-11-11 19:49:05 -0500

    clean up code in main loop
    now we don't have a million nested 'if let's that indent the code all the way to the right
    
    i'm still not completely sure if this is the most rusty way to do this but it's better than before

--- src/main.rs
+++ src/main.rs
@@ -59,54 +59,62 @@ fn main() {
 		let envelope: &imap_proto::types::Envelope = fetch_result.envelope().unwrap();
 		let subject_text = String::from_utf8_lossy(envelope.subject.unwrap_or(b""));
 
-		if let Some(captures) = re.captures(&subject_text) {
-			let report_id = captures.get(1).unwrap().as_str();
-
-			if let Some(bodystructure) = fetch_result.bodystructure() {
-				let report_info = message::find_report(bodystructure, "".to_string());
-				if let Some((part_number, report_type)) = report_info {
-					let section = format!("BODY[{}]", part_number);
-
-					if report_type != types::ReportFileType::Zip {
-						panic!("Non-zip reports not supported yet!");
-					}
-
-					let message_results = dmarc_session.fetch(&fetch_result.message.to_string(), section).unwrap();
-					let message	= message_results.first().unwrap();
-
-					let body_data = message.section(&SectionPath::Part(
-						[ 1 ].to_vec(), None
-					)).unwrap();
-					let body_text = std::str::from_utf8(body_data).unwrap().to_owned();
-					let body_text_no_lines = body_text.replace("\r", "").replace("\n", "");
-
-					if let Ok(decoded_data) = base64::decode(body_text_no_lines.as_bytes()) {
-						let body_reader = Cursor::new(decoded_data);
-
-						let mut archive = zip::ZipArchive::new(body_reader).unwrap();
-
-						if archive.len() != 1 {
-							println!("Couldn't find report file in message for ID {}", report_id);
-							continue;
-						}
-
-						let report_file = archive.by_index(0).unwrap();
-						println!("{}", report_file.name());
-						let file_bytes = report_file.bytes().map(|x| x.unwrap()).collect::<Vec<_>>();
-						println!("{}", String::from_utf8(file_bytes).unwrap());
-					} else {
-						println!("Couldn't decode base64 in message for ID {}", report_id);
-					}
-					return;
-				} else {
-					println!("Couldn't find report archive in message for ID {}", report_id);
-				}
-			} else {
-				println!("Skipping report ID {}", report_id);
-			}
-		} else {
+		// parse the report ID out of the subject
+		let captures = if let Some(x) = re.captures(&subject_text) { x } else {
 			println!("Skipping subject line '{}'", subject_text);
+			continue;
+		};
+		let report_id = captures.get(1).unwrap().as_str();
+
+		// try to get the bodystructure
+		let bodystructure = if let Some(x) = fetch_result.bodystructure() { x } else {
+			println!("Skipping report ID {}", report_id);
+			continue;
+		};
+
+		// find the report
+		let report_info = message::find_report(bodystructure, "".to_string());
+		let (part_number, report_type) = if let Some(x) = report_info { x } else {
+			println!("Couldn't find report archive in message for ID {}", report_id);
+			continue;
+		};
+		if report_type != types::ReportFileType::Zip {
+			panic!("Non-zip reports not supported yet!");
 		}
+
+		// try to get the right part from the message
+		let section = format!("BODY[{}]", part_number);
+		let message_results = dmarc_session.fetch(&fetch_result.message.to_string(), section).unwrap();
+		let message	= message_results.first().unwrap();
+
+		// get the body text
+		// TODO: don't just hardcode this as 1?
+		let body_data = message.section(&SectionPath::Part(
+			[ 1 ].to_vec(), None
+		)).unwrap();
+		let body_text = std::str::from_utf8(body_data).unwrap().to_owned();
+		let body_text_no_lines = body_text.replace("\r", "").replace("\n", "");
+
+		// parse it as base64
+		let decoded_data = if let Ok(x) = base64::decode(body_text_no_lines.as_bytes()) { x } else {
+			println!("Couldn't decode base64 in message for ID {}", report_id);
+			continue;
+		};
+
+		// extract as zip file
+		let body_reader = Cursor::new(decoded_data);
+		let mut archive = zip::ZipArchive::new(body_reader).unwrap();
+
+		if archive.len() != 1 {
+			println!("Couldn't find report file in message for ID {}", report_id);
+			continue;
+		}
+
+		let report_file = archive.by_index(0).unwrap();
+		let file_bytes = report_file.bytes().map(|x| x.unwrap() as char).collect::<String>();
+		println!("{}", file_bytes);
+
+		break;
 	}
 
 	dmarc_session.logout().unwrap();
