78aab902e6f5a4b99bc65e681abdd42a8412b0ff
Alex Studer <alex@studer.dev>
2020-11-24 22:24:22 -0500

    mark all dmarc report fields as public
    this way, they can actually be accessed by the rest of the code

--- src/dmarc/types.rs
+++ src/dmarc/types.rs
@@ -3,89 +3,89 @@ use serde::Deserialize;
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct ReportDateRange {
-	begin: u64,
-	end: u64
+	pub begin: u64,
+	pub end: u64
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct ReportMetadata {
-	org_name: String,
-	email: String,
-	extra_contact_info: String,
-	report_id: String,
-	date_range: ReportDateRange,
-	error: String
+	pub org_name: String,
+	pub email: String,
+	pub extra_contact_info: String,
+	pub report_id: String,
+	pub date_range: ReportDateRange,
+	pub error: String
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct Policy {
-	domain: String,
-	adkim: String,
-	aspf: String,
-	p: String,
-	sp: String,
-	pct: u8
+	pub domain: String,
+	pub adkim: String,
+	pub aspf: String,
+	pub p: String,
+	pub sp: String,
+	pub pct: u8
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct RecordRowPolicy {
-	disposition: String,
-	dkim: String,
-	spf: String,
+	pub disposition: String,
+	pub dkim: String,
+	pub spf: String,
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct RecordRow {
-	source_ip: String,
-	count: u32,
-	policy_evaluated: RecordRowPolicy
+	pub source_ip: String,
+	pub count: u32,
+	pub policy_evaluated: RecordRowPolicy
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct RecordIdentifiers {
-	header_from: String
+	pub header_from: String
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct RecordDKIMResult {
-	domain: String,
-	result: String,
-	selector: String
+	pub domain: String,
+	pub result: String,
+	pub selector: String
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct RecordSPFResult {
-	domain: String,
-	scope: String,
-	result: String
+	pub domain: String,
+	pub scope: String,
+	pub result: String
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct RecordResults {
-	dkim: Vec<RecordDKIMResult>,
-	spf: Vec<RecordSPFResult>,
+	pub dkim: Vec<RecordDKIMResult>,
+	pub spf: Vec<RecordSPFResult>,
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct Record {
-	row: RecordRow,
-	identifiers: RecordIdentifiers,
-	auth_results: RecordResults
+	pub row: RecordRow,
+	pub identifiers: RecordIdentifiers,
+	pub auth_results: RecordResults
 }
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(default)]
 pub struct Report {
-	report_metadata: ReportMetadata,
-	policy_published: Policy,
-	record: Vec<Record>
+	pub report_metadata: ReportMetadata,
+	pub policy_published: Policy,
+	pub record: Vec<Record>
 }
